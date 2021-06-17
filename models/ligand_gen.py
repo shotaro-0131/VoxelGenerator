256d37823e1666e88051c72168b156c67bd7dbb0
shotaro-0131 <shotaro8991@icloud.com>
2021-06-17 10:26:39 +0900

    install RPN

--- models/ligand_gen.py
+++ models/ligand_gen.py
@@ -116,7 +116,38 @@ class Decoder(nn.Module):
         x = self.sigmoid(x)
         return x
 
+class RPN(nn.Module):
+    def __init__(self, grid_size):
+        super(RPN, self).__init__()
+        self.convs = nn.ModuleList(
+            [nn.Conv2D(128, 128, 3, 2, 1) if i%3==0 else nn.Conv2D(128, 128, 3, 1, 1) for i in range(9)])
+        self.deconvs = nn.ModuleList(
+            [nn.ConvTranpose2D(128, 256, 3, 1, 0),
+            nn.ConvTranpose2D(128, 256, 2, 2, 0),
+            nn.ConvTranspose2D(256, 256, 4, 4, 0)])
+        self.cconv = nn.Conv2D(768, 2, 1, 1)
+        self.rconv = nn.Conv2D(768, 4, 1, 1)
+        
+    def forward(self, x):
+        #rpn
+        #block1 256
+        for i in range(3):
+            x = self.convs[i](x)
+        feat1 = self.deconvs[0](x)
+        #block2 256
+        for i in range(3,6):
+            x = self.convs[i](x)
+        feat2 = self.deconvs[1](x)
+        #block3 256
+        for i in range(6,9):
+            x = self.convs[i](x)
+        feat3 = self.deconvs[2](x)
+        feat = torch.cat(feat1, feat2, feat3)
+        clf = self.cconv(feat)
+        reg = self.rconv(feat)
+        return clf, reg
 
+    
 class AutoEncoder(nn.Module):
     def __init__(self, voxel_size):
         super(AutoEncoder, self).__init__()
