ee85aed843e9141a6e38304c2c2f81d0afa02d77
mu <murashow0131@gmail.com>
2021-12-21 13:18:22 +0900

    fix model_fine

--- model_fine.py
+++ model_fine.py
@@ -13,6 +13,7 @@ import optuna
 from optuna.integration import PyTorchLightningPruningCallback
 from joblib import parallel_backend, Parallel, delayed
 from multiprocessing import Process
+from omegaconf import DictConfig, OmegaConf
 
 class AttributeDict(object):
     def __init__(self, obj):
@@ -79,38 +80,52 @@ class WrapperModel(pl.LightningModule):
     
     def _save_model(self, *_):
         pass 
-    def training_epoch_end(self, training_step_outputs):
-        torch.save(self.model.state_dict(), "test.pth")
+    #def training_epoch_end(self, training_step_outputs):
+        #torch.save(self.model.state_dict(), "test.pth")
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
 
+    def get_progress_bar_dict(self):
+        items = super().get_progress_bar_dict()
+        # discard the version number
+        # convert the loss in the proper format
+        items["gpu_id"] = GPU_ID
+        items["node_id"] = RANK
+        return items
+
 GPU_ID=0
+RANK=0
 @hydra.main(config_name="params.yaml")
 def main(cfg: DictConfig) -> None:
+    is_success = False
+    while not is_success:
+        try:
 
-    gpu_id=GPU_ID
-    pl.seed_everything(0)
-    device = torch.device("cuda:{}".format(gpu_id)) if torch.cuda.is_available() else "cpu"
-    data = pd.read_csv(os.path.join(
-        hydra.utils.to_absolute_path(""), cfg.dataset.train_path))
-    print(1.0/27.0)
-    loss = VoxelLoss(1.0/27.0)
-    # seed=random.sample(range(10000), k=5000)
-    # dataloader = DataLoader(
-    #     DataSet(data["pdb_id"].values[seed],
-    #             cfg.preprocess.cell_size, cfg.preprocess.grid_size, False, True), batch_size=cfg.training.batch_size, num_workers=4)
-    val_dataloader = DataLoader(
-        DataSet(data["pdb_id"].values[11000:12000],
-                cfg.preprocess.cell_size, cfg.preprocess.grid_size, False, False), batch_size=cfg.training.batch_size)
-
+            gpu_id=GPU_ID
+            pl.seed_everything(0)
+            device = torch.device("cuda:{}".format(gpu_id)) if torch.cuda.is_available() else "cpu"
+            data = pd.read_csv(os.path.join(
+                hydra.utils.to_absolute_path(""), cfg.dataset.train_path))
+            loss = VoxelLoss(1.0/27.0)
+            # seed=random.sample(range(10000), k=5000)
+            pdb_id_header = "pdb_id"
+            test_used = pd.read_csv(os.path.join(
+                hydra.utils.to_absolute_path(""), cfg.dataset.test_path))
+            data = data[~data[pdb_id_header].isin(test_used)]
+            print(data)
+            dataloader = DataLoader(
+                DataSet(data["pdb_id"].values[:5000],
+                        cfg.preprocess.cell_size, cfg.preprocess.grid_size, True, True), batch_size=cfg.training.batch_size, num_workers=4)
+            val_dataloader = DataLoader(
+                DataSet(data["pdb_id"].values[5000:7500],
+                        cfg.preprocess.cell_size, cfg.preprocess.grid_size, True, False), batch_size=cfg.training.batch_size)
+            is_success=True
+        except Exception as e:
+            print(e)
     def objective(trial: optuna.trial.Trial):
-        seed=random.sample(range(11000), k=5000)
-        dataloader = DataLoader(
-            DataSet(data["pdb_id"].values[seed],
-                    cfg.preprocess.cell_size, cfg.preprocess.grid_size, False, True), batch_size=cfg.training.batch_size, num_workers=4)
-    
-        block_num = trial.suggest_int("block_num", 2, 2, log=True)
+
+        block_num = trial.suggest_int("block_num", 3, 4, log=True)
         kernel_size = trial.suggest_int("kernel_size", 3, 7, step=2)
         pool_type = trial.suggest_categorical("pool", ["max", "ave"])
         pool_kernel_size = trial.suggest_int("pool_kernel_size", 2, 2, log=True)
@@ -121,17 +136,19 @@ def main(cfg: DictConfig) -> None:
 
         lr = trial.suggest_loguniform("lr", 1e-5, 1e-1)
 
-        drop_out = trial.suggest_uniform("drop_out", 0.0, 1.0)
+        epoch = trial.suggest_int("epoch", 10, 20, step=10)
+
+        #drop_out = trial.suggest_uniform("drop_out", 0.0, 1.0)
 
         hyperparameters = dict(block_num=block_num, kernel_size=kernel_size, f_map=f_map, pool_type=pool_type, pool_kernel_size=pool_kernel_size,
-                            in_channel=7, out_channel=3, lr=lr, drop_out=drop_out, gpu_id=gpu_id)
+                            in_channel=7, out_channel=3, lr=lr, gpu_id=gpu_id, epoch=epoch)
 
         print(hyperparameters)
         model = UNet(AttributeDict(hyperparameters)).to(device)
         # model = torch.nn.DataParallel(
         #     model) if cfg.training.gpu_num > 1 else model
-        trainer = pl.Trainer(max_epochs=cfg.training.epoch,
-                             progress_bar_refresh_rate=20,
+        trainer = pl.Trainer(max_epochs=epoch,
+                             progress_bar_refresh_rate=100,
                              gpus=[gpu_id],
                              logger=False,
                              checkpoint_callback=False,
@@ -152,11 +169,21 @@ def main(cfg: DictConfig) -> None:
         return trainer.callback_metrics["val_loss"].item()
 
     #pruner = optuna.pruners.PercentilePruner(60)
-    pruner = optuna.pruners.MedianPruner(n_warmup_steps=cfg.training.epoch//2, n_startup_trials=10)
-    study = optuna.create_study(direction='minimize', load_if_exists=True, pruner=pruner, storage="sqlite:///t.db", study_name="unet")
-    #with parallel_backend("multiprocessing", n_jobs=cfg.training.gpu_num):
-    # study.optimize(objective, timeout=8*60*55, gc_after_trial=True)
-    study.optimize(objective, n_trials=1, gc_after_trial=True)
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=10)
+    is_success=False
+    while not is_success:
+        try:
+            study = optuna.create_study(direction='minimize', load_if_exists=True, pruner=pruner, storage="sqlite:///test2.db", study_name="unet")
+
+            #with parallel_backend("multiprocessing", n_jobs=cfg.training.gpu_num):
+            study.optimize(objective, timeout=60*60*8, gc_after_trial=True)
+
+ 
+                
+            is_success=True
+
+        except Exception as e:
+            print(e)
     print("Number of finished trials: {}".format(len(study.trials)))
 
     print("Best trial:")
@@ -168,19 +195,30 @@ def main(cfg: DictConfig) -> None:
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
-
 import time
 # from mpi4py import MPI
 
 if __name__ == "__main__":
 
     # comm = MPI.COMM_WORLD
+    # RANK = comm.Get_rank()
     # rank = comm.Get_rank()
     # GPU_ID=rank
     # time.sleep(rank)
-    workers = [Process(target=main) for i in range(3)]
-    for worker in workers:
-        time.sleep(10)
-        worker.start()
     # main()
+
+    # workers = [Process(target=main) for i in range(OmegaConf.load(os.path.join(hydra.utils.to_absolute_path(""), "params.yaml")).training.gpu_num)]
+    # for i, worker in enumerate(workers):
+    #     # time.sleep(10)
+    #     GPU_ID = i
+    #     worker.start()
+
+    processes = []
+    for rank in range(OmegaConf.load(os.path.join(hydra.utils.to_absolute_path(""), "params.yaml")).training.gpu_num):
+        p = torch.multiprocessing.Process(target=main)
+        GPU_ID = rank
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
 
